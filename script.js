24f26d526573e5fb6685070089ab2d139e78fc40
Connor Johnson <connorjohnson@connors-air.mynetworksettings.com>
2024-03-21 20:46:24 -0400

    made the textAreas pull from localStorage so that events are properly saved

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ saveButtons = Array.prototype.slice.call( document.getElementsByClassName("btn")
 
 function start () {
   currentDay.textContent = getCurrentDate();
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+
+  for (var i = 0; i < localStorage.length; i++){
+    var key = localStorage.key(i);
+    if (key !== null && key.includes("hour-")){
+      for (r of hourRows){
+        if (r.id === key){
+          r.children[1].value = localStorage.getItem(key);
+        }
+      }
+    }
+}
 
   saveButtons.forEach(b => {
     b.addEventListener("click", function(){
-      //console.log(`${b.parentElement.children[0].innerHTML} textArea pressed`);
       localStorage.setItem(b.parentElement.id, b.parentElement.children[1].value);
-      //console.log(b.parentElement.children[1].value);
     });
   });
  
@@ -33,12 +35,6 @@ function start () {
       r.className = "row time-block future";
     }
   }
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 };
 
 function getCurrentDate(){
@@ -56,6 +52,8 @@ function timeStatus(timeString){
   if (AMPM === 'PM' && hour < 12){
     hour += 12;
   }
+
+  curHour = 12;
 
   if(curHour > hour){
     return -1;
